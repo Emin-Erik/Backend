878b6084e9240c7b7a5abc5ffc3d61a2bffd74e6
Erik <gheriserik@gmail.com>
2023-10-22 21:57:56 +0200

    added provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Provider from "./components/Provider";
+import { ClientCookiesProvider } from "./provider";
+import { cookies } from "next/headers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,10 +18,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        <Provider>{children}</Provider>
-      </body>
-    </html>
+    <ClientCookiesProvider value={cookies().getAll()}>
+      <html lang="en">
+        <body className={inter.className}>
+          <Provider>{children}</Provider>
+        </body>
+      </html>
+    </ClientCookiesProvider>
   );
 }
