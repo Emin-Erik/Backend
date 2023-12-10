2ba2f28a20854b88a963e8915ad5a4b641bb1178
dopaemin <oklopcicemin@gmail.com>
2023-12-10 15:24:43 +0100

    Passwort validation

--- src/app/lib/schema.ts
+++ src/app/lib/schema.ts
@@ -3,7 +3,14 @@ import { z } from "zod";
 export const FormDataSchema = z
   .object({
     Name: z.string().min(1, "Deine Name wird benötigt"),
-    Password: z.string().min(8, "Dein Passwort muss mind. 8 Zeichen lang sein"),
+    Password: z
+      .string()
+      .min(8, "Dein Passwort muss mind. 8 Zeichen lang sein")
+      .regex(
+        /[A-Z]/,
+        "Das Passwort muss mindestens einen Großbuchstaben enthalten"
+      )
+      .regex(/[0-9]/, "Das Passwort muss mindestens eine Zahl enthalten"),
     confirmPassword: z
       .string()
       .min(8, "Dein Passwort muss mind. 8 Zeichen lang sein"),
