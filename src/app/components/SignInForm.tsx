5819fee6830d7610b92565fd37ecb4386c842b4e
Erik <gheriserik@gmail.com>
2024-01-01 22:03:19 +0100

    added facebook login

--- src/app/components/SignInForm.tsx
+++ src/app/components/SignInForm.tsx
@@ -1,11 +1,20 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import { signIn, useSession } from "next-auth/react";
+import { getProviders, signIn, useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import { Link, Input, Button } from "@nextui-org/react";
+import type {
+  GetServerSidePropsContext,
+  InferGetServerSidePropsType,
+} from "next"
+import { getServerSession } from "next-auth/next"
+import { authOptions } from "../api/auth/[...nextauth]/auth"
 
-const SignInForm = () => {
+export default function SignInForm ({
+                                      providers,
+                                    }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+
   const router = useRouter();
 
   const { status } = useSession();
@@ -45,6 +54,7 @@ const SignInForm = () => {
   }, [status]);
 
   return (
+      <>
     <div className="flex flex-col gap-4 bg-gray-400 p-4">
       <Input
         type="text"
@@ -61,7 +71,29 @@ const SignInForm = () => {
       <Link href="#">Default Link</Link>
       <p>{message}</p>
     </div>
+        {Object.values(providers).map((provider) => (
+            <div key={provider.name}>
+              <button onClick={() => signIn(provider.id)}>
+                Sign in with {provider.name}
+              </button>
+            </div>
+        ))}
+      </>
   );
-};
+}
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const session = await getServerSession(context.req, context.res, authOptions)
 
-export default SignInForm;
+  // If the user is already logged in, redirect.
+  // Note: Make sure not to redirect to the same page
+  // To avoid an infinite loop!
+  if (session) {
+    return { redirect: { destination: "/" } }
+  }
+
+  const providers = await getProviders()
+
+  return {
+    props: { providers: providers ?? [] },
+  }
+}
