27f7de6ee00eeae32b815503d8416ffb56fde83b
dopaemin <oklopcicemin@gmail.com>
2024-02-26 20:38:28 +0100

    changes

--- src/app/webapp/page.tsx
+++ src/app/webapp/page.tsx
@@ -4,14 +4,24 @@ import WebAppNavbarNew from "./webAppNavBar/webAppNavbar_new";
 const WebApp = () => {
   return (
     <div
-      className="bg-cover"
-      style={{
-        backgroundImage: 'url("https://wallpapercave.com/wp/wp8207303.jpg")',
-      }}
-    >
-      <WebAppNavbarNew />
-      <WebPage />
+  className="bg-cover h-screen w-screen"
+  style={{
+    backgroundImage: 'url("https://wallpapercave.com/wp/wp8207303.jpg")',
+  }}
+>
+  <WebAppNavbarNew />
+  <WebPage />
+  {/** Footer */}
+  <div className="absolute bottom-0 left-0 right-0">
+    <div className="mx-auto w-full max-w-screen-xl px-6">
+    <div className="h-12 rounded-lg bg-black/25 drop-shadow-2xl flex items-center justify-center absolute bottom-0 left-0 right-0">
+  © Axiom 2024
+</div>
+
     </div>
+  </div>
+</div>
+
   );
 };
 
--- src/app/webapp/webAppDashboard/page.tsx
+++ src/app/webapp/webAppDashboard/page.tsx
@@ -11,5 +11,16 @@ export const metadata: Metadata = {
 };
 
 export default function WebPage() {
-  return <></>;
+  return (
+<>
+  <div className="grid grid-cols-2 gap-6">
+    <div className="mt-6 ml-6 mb-12 h-[12rem] w-[10rem] drop-shadow-2xl rounded-lg bg-white/30"></div>
+    <div className="mt-6 mb-12 h-[12rem] w-[10rem] drop-shadow-2xl rounded-lg bg-white/30"></div>
+  </div>
+
+  <div className="mt-2 ml-6 mb-8 mr-6 h-[30rem] w-[23rem] drop-shadow-2xl rounded-lg bg-black/25"></div>
+</>
+
+  
+  );
 }
